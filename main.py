d7d32ee4f3176a4c762f5ba127b4817a1deb67eb
David Graper <dgraper@albany.edu>
2022-01-12 10:19:18 -0500

    Commit that includes modular "UADW_CRSE_BY_ACAD_ORG" reports

--- main.py
+++ main.py
@@ -7,18 +7,41 @@ import pyautogui
 import time
 import os
 import datetime
+import glob
 from datetime import date
 
 def renamedownloadedfile_milestone(newfilename):
     renamedfile = "c:\\Users\\dg145254\\Downloads\\" + newfilename + "_" \
-                  + datetime.date.today().strftime("%m%d%y") + ".xml"
-    os.rename("c:\\Users\\dg145254\\Downloads\\UASR_MILESTN_PROG.xml", renamedfile)
+                  + datetime.date.today().strftime("%m%d%y") + ".xlsx"
+    os.rename("c:\\Users\\dg145254\\Downloads\\UASR_MILESTN_PROG.xlsx", renamedfile)
 
 
-def renamedownloadedfile_coursebyacadorg(newfilename):
-    renamedfile = "c:\\Users\\dg145254\\Downloads\\" + newfilename + "_" \
-                  + datetime.date.today().strftime("%m%d%y") + ".xml"
-    os.rename("c:\\Users\\dg145254\\Downloads\\UADW_CRSE_BY_ACAD_ORG.xml", renamedfile)
+def renamedownloadedfile_coursebyacadorg(oldfilenamepattern, appendedinfo1, appendedinfo2):
+
+    # Get name of file to be renamed
+    oldfilename = findfilenameindir("c:\\Users\\dg145254\\Downloads\\", oldfilenamepattern)
+
+    # Create a more descriptive new file name
+    newname ="UADW_CRSE_BY_ACAD_ORG_" + appendedinfo1 + "_" + appendedinfo2
+    renamedfile = "c:\\Users\\dg145254\\Downloads\\" + newname + "_" \
+                  + datetime.date.today().strftime("%m%d%y") + ".xlsx"
+
+    # rename file
+    os.rename(oldfilename, renamedfile)
+
+
+def findfilenameindir(pathtosearch, filenamebeginningchars):
+
+    searchstring = pathtosearch + filenamebeginningchars + "_[0-9]*.xlsx"
+
+    files = glob.glob(searchstring)
+
+    if len(files) != 1:
+        print("TOO MANY OUTPUT FILES")
+        exit()
+
+    return files[0]
+
 
 def downloadmilestonereports():
 
@@ -118,7 +141,6 @@ def downloadmilestonereports():
 
 def downloadcoursebyacadorgreports():
 
-    # webbrowser.open("https://www.google.com")
     webbrowser.open("https://ps.itsli.albany.edu/psp/ps92prod/?cmd=login&languageCd=ENG&")
     time.sleep(5)
     pyautogui.write("dg145254")
@@ -143,11 +165,17 @@ def downloadcoursebyacadorgreports():
     pyautogui.click(79, 291)
     time.sleep(6)
 
-    # terms = ['2101', '2103', '2106', '2109', '2111', '2113', '2116', '2119', '2121', '2123', '2126', '2129', '2131',
-    #          '2133', '2136', '2139', '2141', '2143', '2146', '2149', '2151', '2153', '2156', '2159', '2161', '2163',
-    #          '2166', '2169', '2171', '2173', '2176', '2179', '2181', '2183', '2186', '2189', '2191', '2193', '2196',
-    #          '2199', '2201', '2203', '2206', '2209', '2211', '2213', '2216', '2219']
-    terms = ['2213', '2216', '2219']
+    # # terms = ['2101', '2103', '2106', '2109', '2111', '2113', '2116', '2119', '2121', '2123', '2126', '2129', '2131',
+    # #          '2133', '2136', '2139', '2141', '2143', '2146', '2149', '2151', '2153', '2156', '2159', '2161', '2163',
+    # #          '2166', '2169', '2171', '2173', '2176', '2179', '2181', '2183', '2186', '2189', '2191', '2193', '2196',
+    # #          '2199', '2201', '2203', '2206', '2209', '2211', '2213', '2216', '2219']
+    # #          '2199', '2201', '2203', '2206', '2209', '2211', '2213', '2216', '2219']
+    # terms = ['2166', '2169', '2171', '2173', '2176', '2179', '2181', '2183', '2186', '2189', '2191', '2193', '2196']
+    # terms = ['2199']
+    # terms = ['2201', '2203', '2206', '2209', '2211', '2213', '2216']
+    # terms = ['2151', '2153', '2156', '2159', '2161', '2163']
+    # terms = ['2133', '2136', '2139', '2141', '2143', '2146', '2149']
+    terms = ['2101', '2103', '2106', '2109', '2111', '2113', '2116', '2119', '2121', '2123', '2126', '2129', '2131']
     plan10s = ['BIO-MS', 'BIO-MSBCP', 'BIO-MSCMB', 'BIO-MSCT', 'BIO-MSFBM', 'BIO-MSFMB', 'BIO-MSFOR',
                'BIO-NON', 'BIO-PHD']
 
@@ -156,14 +184,13 @@ def downloadcoursebyacadorgreports():
 
         for plan10 in plan10s:
 
-            GetAcadOrgReport(term, "BIOSCI", plan10)
-            renamedownloadedfile_coursebyacadorg("UADW_CRSE_BY_ACAD_ORG_" + plan10 + "_" + term)
+            print("TERM = " + term)
+            print("PLAN = " + plan10)
+            print(".")
 
-        # GetAcadOrgReport("2219", "BIOSCI", "BIO-MSBCP")
-        # renamedownloadedfile_coursebyacadorg("UADW_CRSE_BY_ACAD_ORG_BIO-MSBCP")
-        #
-        # GetAcadOrgReport("2219", "BIOSCI", "BIO-MSFBM")
-        # renamedownloadedfile_coursebyacadorg("UADW_CRSE_BY_ACAD_ORG_BIO-MSFBM")
+            GetAcadOrgReport(term, "BIOSCI", plan10)
+            originalfilenamepattern = "UADW_CRSE_BY_ACAD_ORG"
+            renamedownloadedfile_coursebyacadorg(originalfilenamepattern, plan10, term)
 
 
 def GetAcadOrgReport(term, acadorg, plan10):
@@ -171,7 +198,9 @@ def GetAcadOrgReport(term, acadorg, plan10):
     # Longish wait
     time.sleep(6)
     # Click on "XML" output link
-    pyautogui.click(706, 487)
+    # pyautogui.click(706, 487)
+    # Click on "Excel" output link
+    pyautogui.click(669,492)
     time.sleep(6)
     # Select "Career"
     pyautogui.click(282, 132)
@@ -207,6 +236,10 @@ def print_hi(name):
 
     # downloadmilestonereports()
     downloadcoursebyacadorgreports()
+
+    # # Get name of file to be renamed
+    # oldfilenamepattern = "UADW_CRSE_BY_ACAD_ORG"
+    # oldfilename = findfilenameindir("c:\\Users\\dg145254\\Downloads\\", oldfilenamepattern)
 
     while 1==1:
         time.sleep(2)
